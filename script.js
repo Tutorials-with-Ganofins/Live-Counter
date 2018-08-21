af35e668b90ed1b155eedc876889f2a1f7081823
Ganesh Bagaria <11516116+Ganofins@users.noreply.github.com>
2018-08-21 16:14:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function result(){
-				var channel_name = "";
-				var sub_count = "";
-				var channel_id = document.getElementById("channel_id").value;
-				var xhttp = new XMLHttpRequest();
+	var channel_name = "";
+	var sub_count = "";
+	var channel_id = document.getElementById("channel_id").value;
+	var xhttp = new XMLHttpRequest();
 				
-				xhttp.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
-						var output = this.responseText;
-						output = JSON.parse(output);
-						channel_name = output.items[0].snippet["title"];
-						sub_count = output.items[0].statistics["subscriberCount"];
-						document.getElementById("demo").innerHTML = channel_name+sub_count;
-					}
-				};
-				xhttp.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id="+channel_id+"&key=AIzaSyCBgy9GTrM6PCPAgYwnX_vqZpjiVa5TtC8", true);
-				xhttp.send();
-				setTimeout(result,1000);
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+				var output = this.responseText;
+				output = JSON.parse(output);
+				channel_name = output.items[0].snippet["title"];
+				sub_count = output.items[0].statistics["subscriberCount"];
+				document.getElementById("channel_name").innerHTML = channel_name;
+				document.getElementById("sub_count").innerHTML = sub_count;
+			}
+		};
+	xhttp.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id="+channel_id+"&key=AIzaSyCBgy9GTrM6PCPAgYwnX_vqZpjiVa5TtC8", true);
+	xhttp.send();
+	setTimeout(result,1000);
 }
