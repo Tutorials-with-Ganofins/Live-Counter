55b0e6ba8e414ed7ad2a13af2ed9e339a5c62a1d
Ganofins <bagariaganesh@gmail.com>
2019-05-25 15:37:01 +0530

    more changes

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-function result(){
-	
-	var channel_id = document.getElementById("channel_id").value;
+function result(channel_id)
+{
 	if (channel_id === "")
 	{
 		return alert("Please enter the Channel ID");
 	}
-	document.getElementById("sub_count").style.display = "inline";
-	var channel_name = "";
-	var sub_count = "";
-	var xhttp = new XMLHttpRequest();
+	document.getElementById("channel_id").value = "";
 	
-	//for the request			
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-				var output = this.responseText;
-				output = JSON.parse(output);
-				channel_name = output.items[0].snippet["title"];
-				sub_count = output.items[0].statistics["subscriberCount"];
-				document.getElementById("channel_name").innerHTML = channel_name;
-				document.getElementById("sub_count").innerHTML = sub_count;
-			}
-		};
+	//for the request
+	request(channel_id);
+}
+
+function request(channel_id)
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4 && this.status == 200)
+		{
+			var output = this.responseText;
+			output = JSON.parse(output);
+			document.getElementById("channel_name").innerHTML = output.items[0].snippet["title"];
+			document.getElementById("sub_count").style.display = "inline";
+			document.getElementById("sub_count").innerHTML = output.items[0].statistics["subscriberCount"];
+		}
+		else if(this.readyState == 4)
+		{
+			return console.log(JSON.parse(this.responseText)["error"]["message"])
+		}
+	};
 	xhttp.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id="+channel_id+"&key=AIzaSyCBgy9GTrM6PCPAgYwnX_vqZpjiVa5TtC8", true);
 	xhttp.send();
-	setTimeout(result,1000);
+	setTimeout(request(), 1000);
 }
